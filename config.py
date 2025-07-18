174a05a29006f1c63543788a3406dbfc6e3cc5fb
Jishanoiq <jannatsj04@gmail.com>
2025-07-19 04:32:57 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+# config.py
 
+BOT_TOKEN = "7819015560:AAF6l-6d3rXtGXn7GG0mIa-MkQkZe-exPC8"
+CHANNEL_ID = "@otpbroadcast69"
+
+# Example API config (replace if you're using IVAS etc.)
+OTP_API_URL = "https://example.com/api/get-otp"
+OTP_API_KEY = "your_api_key_here"
